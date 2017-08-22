1fde761c9d85b47ee75842a5e9e16164531f7ea9
mng16 <mairi.ng.16@ucl.ac.uk>
2017-08-22 08:52:00 +0100

    changed time formatting

--- mhtBot/app.js
+++ mhtBot/app.js
@@ -23,6 +23,13 @@ var dateFormatLite = require('date-format-lite');
 
 var bcrypt = require('bcrypt');
 
+
+//====================
+// Required for time
+//====================
+
+var local = false;
+
 //============
 // Constants
 //============
@@ -322,8 +329,12 @@ bot.dialog('login', [
 
 function getBotMsgTime(session){
 	var botTime = new Date(session.userData.lastMessageSent);
-	var botTimeFormatted = dateFormat(botTime, "yyyy-mm-dd HH:MM:ss");
-	//var botTimeFormatted = botTime.format("yyyy-mm-dd HH:MM:ss");
+	var botTimeFormatted = null;
+	if(local == true){
+		botTimeFormatted = dateFormat(botTime, "yyyy-mm-dd HH:MM:ss");
+	}else{
+		botTimeFormatted = botTime.format("yyyy-mm-dd HH:MM:ss");
+	}
 
 	console.log("Bot messaged at: " + botTimeFormatted);
 	return botTimeFormatted;
@@ -331,8 +342,12 @@ function getBotMsgTime(session){
 
 function getUserMsgTime(session){
 	var userTime = new Date(session.message.localTimestamp);
-	var userTimeFormatted = dateFormat(userTime, "yyyy-mm-dd HH:MM:ss");
-	//var userTimeFormatted = userTime.format("yyyy-mm-dd HH:MM:ss");
+	var userTimeFormatted = null;
+	if(local == true){
+		userTimeFormatted = dateFormat(userTime, "yyyy-mm-dd HH:MM:ss");
+	}else{
+		userTimeFormatted = userTime.format("yyyy-mm-dd HH:MM:ss");
+	}
 
 	console.log("User responded at: " + userTimeFormatted);
 	return userTimeFormatted;
