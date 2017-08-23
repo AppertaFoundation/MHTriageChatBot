c6da2e6c1509994b52fb1fe68e1cabd68a27f837
mng16 <mairi.ng.16@ucl.ac.uk>
2017-08-23 23:35:24 +0100

    updated nav

--- mhtBotDBWebsite/includes/nav.php
+++ mhtBotDBWebsite/includes/nav.php
@@ -10,7 +10,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">MhtBot Database Access</a>
+			<a class="navbar-brand" href="../public/index.php">MhtBot Database Access</a>
 		</div>
 		
 	        <span class="navbar-toggler-icon"></span>
@@ -20,8 +20,8 @@
 			<ul class="nav navbar-nav">
 				<li><a href="index.php">Home</a><li>
 				<li><a href="../public/allUsers.php">All Users</a></li>
-				<li><a href="highScoringUsers.php">High Scoring Users</a></li>
-				<li><a href="harmRisk.php">Users at Risk of Harm</a></li>
+				<li><a href="../public/highScoringUsers.php">High Scoring Users</a></li>
+				<li><a href="../public/harmRisk.php">Users at Risk of Harm</a></li>
 			</ul>
 		</div><!-- /.navbar-collapse -->
 	</div><!-- /.container-fluid -->
