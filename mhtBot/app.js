0e757d1e4c835271fab3f87ac2bb373aeb4fb5c3
mng16 <mairi.ng.16@ucl.ac.uk>
2017-09-03 11:25:59 +0100

    unused files removed

--- mhtBot/app.js
+++ mhtBot/app.js
@@ -115,7 +115,7 @@ var feeling = null;
 var local = false;
 
 // to set session.delay() to make SMS messages come through in correct order.
-var delayTime = 1000;
+var delayTime = 1;
 
 
 //=============
@@ -158,101 +158,6 @@ bot.dialog('greeting', [
 			session.userData.usernameValid = true;
 			session.beginDialog('register');
 		}
-	}
-]);
-
-//------------------//
-// Login Dialog
-//------------------//
-bot.dialog('login', [
-	function(session){
-		session.sendTyping();
-		session.delay(delayTime);
-		builder.Prompts.text(session, "Please enter your username:");
-	},
-	function(session,results, next){
-		session.userData.username = results.response;
-		console.log("Username entered was " + session.userData.username);
-
-		request = new Request(
-			"SELECT UserID FROM Users WHERE Username = " + mysql.escape(session.userData.username), function(err, rowCount, rows){
-				console.log("In query for Username");
-				if(!err){
-					console.log("Query to check if username exists successfully executed");
-					console.log(rowCount + " rows returned");
-					if(rowCount>0){
-						console.log("Username " + session.userData.username + " exists.");
-						next();
-					}else{
-						console.log("Username " + session.userData.username + " does not exist");
-						session.sendTyping();
-						session.delay(delayTime);
-						session.send("I'm sorry, I don't recognise that username. Please try logging in again.");
-						session.beginDialog('login');
-					}
-				}else{
-					console.log("An error occurred in checking whether this username exists." + err);
-					//session.endDialog("User does not exist on system");
-				}
-			}
-		);
-		connection.execSql(request);
-	},
-	function(session){
-		session.sendTyping();
-		session.delay(delayTime);
-		builder.Prompts.text(session, "Thanks. Now please enter your password.");
-	},
-	function(session, result){
-		session.userData.password = result.response;
-		var plainTextPassword = session.userData.password;
-
-		request = new Request(
-			"SELECT UserID, Password FROM Users WHERE Username =" +  mysql.escape(session.userData.username),
-				function(err, rowCount, rows){
-					if(!err){
-						console.log("Query to retrieve UserID and Password from db successful.");
-						console.log(rowCount + " rows returned.");
-						if(rowCount>0){
-							console.log("UserID and Password successfully retrieved from database");
-						}else{
-							console.log("No userID and password retrieved. This should not happen.");
-						}
-					}else{
-						console.log("Error in retrieving UserID and Password from database: " + err);
-				}
-			}
-		);
-
-		request.on('row', function(columns){
-			console.log("Logged in user userID is: " + columns[0].value);
-			console.log("Password from db is: " + columns[1].value);
-
-			var hash = columns[1].value;
-			
-			bcrypt.compare(plainTextPassword, hash, function(err, res){
-				if(res === true){
-					console.log("Password entered matches password stored in database");
-
-					session.userData.password = columns[1].value;
-					session.userData.userID = columns[0].value;
-
-					console.log("User %s logged in.", session.userData.username);
-					session.sendTyping();
-					session.delay(delayTime);
-					session.endDialog("Wecome back %s!", session.userData.username);
-					session.beginDialog('generalQs');
-					//session.beginDialog('gad7');  for testing 
-				}else{
-					console.log("Passwords do not match");
-					session.sendTyping();
-					session.delay(delayTime);
-					session.send("I'm sorry, your password is incorrect. Please try logging in again");
-					session.beginDialog('login');
-				}
-			});
-		});
-		connection.execSql(request);
 	}
 ]);
 
@@ -351,7 +256,100 @@ bot.dialog('register', [
 	}, 
 ]);
 
+//------------------//
+// Login Dialog
+//------------------//
+bot.dialog('login', [
+	function(session){
+		session.sendTyping();
+		session.delay(delayTime);
+		builder.Prompts.text(session, "Please enter your username:");
+	},
+	function(session,results, next){
+		session.userData.username = results.response;
+		console.log("Username entered was " + session.userData.username);
 
+		request = new Request(
+			"SELECT UserID FROM Users WHERE Username = " + mysql.escape(session.userData.username), function(err, rowCount, rows){
+				console.log("In query for Username");
+				if(!err){
+					console.log("Query to check if username exists successfully executed");
+					console.log(rowCount + " rows returned");
+					if(rowCount>0){
+						console.log("Username " + session.userData.username + " exists.");
+						next();
+					}else{
+						console.log("Username " + session.userData.username + " does not exist");
+						session.sendTyping();
+						session.delay(delayTime);
+						session.send("I'm sorry, I don't recognise that username. Please try logging in again.");
+						session.beginDialog('login');
+					}
+				}else{
+					console.log("An error occurred in checking whether this username exists." + err);
+					//session.endDialog("User does not exist on system");
+				}
+			}
+		);
+		connection.execSql(request);
+	},
+	function(session){
+		session.sendTyping();
+		session.delay(delayTime);
+		builder.Prompts.text(session, "Thanks. Now please enter your password.");
+	},
+	function(session, result){
+		session.userData.password = result.response;
+		var plainTextPassword = session.userData.password;
+
+		request = new Request(
+			"SELECT UserID, Password FROM Users WHERE Username =" +  mysql.escape(session.userData.username),
+				function(err, rowCount, rows){
+					if(!err){
+						console.log("Query to retrieve UserID and Password from db successful.");
+						console.log(rowCount + " rows returned.");
+						if(rowCount>0){
+							console.log("UserID and Password successfully retrieved from database");
+						}else{
+							console.log("No userID and password retrieved. This should not happen.");
+						}
+					}else{
+						console.log("Error in retrieving UserID and Password from database: " + err);
+				}
+			}
+		);
+
+		request.on('row', function(columns){
+			console.log("Logged in user userID is: " + columns[0].value);
+			console.log("Password from db is: " + columns[1].value);
+
+			var hash = columns[1].value;
+			
+			bcrypt.compare(plainTextPassword, hash, function(err, res){
+				if(res === true){
+					console.log("Password entered matches password stored in database");
+
+					session.userData.password = columns[1].value;
+					session.userData.userID = columns[0].value;
+
+					console.log("User %s logged in.", session.userData.username);
+					session.sendTyping();
+					session.delay(delayTime);
+					session.endDialog("Wecome back %s!", session.userData.username);
+					session.beginDialog('generalQs');
+					//session.beginDialog('gad7');  for testing 
+				}else{
+					console.log("Passwords do not match");
+					session.sendTyping();
+					session.delay(delayTime);
+					session.send("I'm sorry, your password is incorrect. Please try logging in again");
+					session.beginDialog('login');
+				}
+			});
+		});
+		connection.execSql(request);
+	}
+]);
 
 
 //------------------//
