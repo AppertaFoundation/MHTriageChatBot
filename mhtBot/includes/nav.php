b393f2bacc438af0e2fa31a67f2c9ddab070351e
mng16 <mairi.ng.16@ucl.ac.uk>
2017-10-06 22:29:28 +0100

    changes styles folder name

--- mhtBot/includes/nav.php
+++ mhtBot/includes/nav.php
@@ -1,7 +1,7 @@
 <nav>
 	<ul class="nav nav-tabs">
 		<li role="presentation"><a href="index.php">Web Chat</a></li>
-		<li role="presentation"><a href="skype.php"">Skype</a></li>
+		<li role="presentation"><a href="skype.php">Skype</a></li>
 		<li role="presentation"><a href="fbMessenger.php">Facebook Messenger</a></li>
 		<li role="presentation"><a href="sms.php">SMS</a></li>
 	</ul>
---
+++ mhtBot/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Mental Health Triage Chatbot | Web Chat</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<!--HEADER -->
+<header>
+	<div class="page-header">
+	<h1>Mental Health Triage ChatBot<br><small>Say hello to the MhtBot</small></h1>
+	</div>
+</header>
+
+
+<nav>
+	<ul class="nav nav-tabs">
+		<li role="presentation" class="active"><a href="index.html">Web Chat</a></li>
+		<li role="presentation"><a href="skype.html"">Skype</a></li>
+		<li role="presentation"><a href="fbMessenger.html">Facebook Messenger</a></li>
+		<li role="presentation"><a href="sms.html">SMS</a></li>
+	</ul>
+</nav>
+
+<main>
+
+<br>
+
+<div class="chatWindow">
+<iframe src='https://webchat.botframework.com/embed/MHTestBot?s=jDzUIBd3Vwo.cwA.gdo.VrE814IcCY-MfLwqWSmTZ8ehXBog6ScWWmgtLoOA_iI' height="400"></iframe>
+</div>
+</section>
+
+</main>
+
+</body>
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</html>
